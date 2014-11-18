0a1d9e75073d8d77bdae54f7ea7517281bf57d4d
Brendan 'nogthree' Halliday <nog3@nog3.net>
2014-11-18 20:21:53 +1000

    Adding motor test

---
+++ motortest.js
@@ -0,0 +1,15 @@
+var five = require("johnny-five"),
+  board = new five.Board(),
+  sleep = require('sleep');
+
+board.on('ready', function() ) {
+	for (var i = 22; i < 46; i++) {
+
+  var relay = new five.Relay(i);
+
+  relay.on();
+  sleep.sleep(2);
+  relay.off();
+  sleep.sleep(2)    
+  }
+);
