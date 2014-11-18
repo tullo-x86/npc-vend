8a3952ce178a7ebd12f34579c9744ed4e23f5ff7
Brendan 'nogthree' Halliday <nog3@nog3.net>
2014-11-18 20:24:50 +1000

    Fixing again

--- motortest.js
+++ motortest.js
@@ -2,7 +2,7 @@ var five = require("johnny-five"),
   board = new five.Board(),
   sleep = require('sleep');
 
-board.on('ready') {
+board.on("ready", function() {
 	for (var i = 22; i < 46; i++) {
 
   var relay = new five.Relay(i);
